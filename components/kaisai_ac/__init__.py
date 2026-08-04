7d1d9b6e8826c77885a6fce14e4bcbe3907563a9
Nortonko <52453201+Nortonko@users.noreply.github.com>
2026-08-04 14:25:11 +0200

    Add files via upload

--- components/kaisai_ac/__init__.py
+++ components/kaisai_ac/__init__.py
@@ -1,19 +1,18 @@
 import esphome.codegen as cg
-from esphome.components import climate, switch, select
+from esphome.components import climate, switch, select, sensor
 
 CODEOWNERS = ["@Nortonko"]
 DEPENDENCIES = ["uart"]
-AUTO_LOAD = ["switch", "select"]
+AUTO_LOAD = ["switch", "select", "sensor"]
 
 kaisai_ns = cg.esphome_ns.namespace("kaisai_ac")
 
-KaisaiAC = kaisai_ns.class_(
-    "KaisaiAC", climate.Climate, cg.Component
-)
+KaisaiAC = kaisai_ns.class_("KaisaiAC", climate.Climate, cg.Component)
 KaisaiSwitch = kaisai_ns.class_("KaisaiSwitch", switch.Switch, cg.Component)
 KaisaiSelect = kaisai_ns.class_("KaisaiSelect", select.Select, cg.Component)
+KaisaiSensor = kaisai_ns.class_("KaisaiSensor", sensor.Sensor, cg.Component)
 
 KaisaiFunction = kaisai_ns.enum("KaisaiFunction")
 
-# spoločný kľúč, ktorým switch/select odkazujú na hlavný climate komponent
+# spoločný kľúč, ktorým switch/select/sensor odkazujú na hlavný climate komponent
 CONF_KAISAI_ID = "kaisai_ac_id"
